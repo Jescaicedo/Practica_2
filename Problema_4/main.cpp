8d12807835b9c03258a756c9691eb4436bd835fb
Juan Caicedo <jesteban.caicedo@udea.edu.co>
2023-03-22 14:10:17 -0500

    version final problema 4

--- Problema_4/main.cpp
+++ Problema_4/main.cpp
@@ -11,6 +11,20 @@ int main()
     cin>>array;
     N=leernumero(array);
     cout<<N<<endl;
+}
 
-
+int leernumero(char *p)
+{
+    char carac=0, aux=0;
+    int a=0, cont=0, num=0;
+    carac=*(p+cont);
+    while (carac>47 && carac<58){
+        aux=carac;
+        aux-=48;
+        a=aux;
+        num=num*10+a;
+        cont+=1;
+        carac=*(p+cont);
+    }
+    return num;
 }
