27d93ac69845bc6c788209810eae52de38aedb45
Juan Caicedo <jesteban.caicedo@udea.edu.co>
2023-03-22 15:43:16 -0500

    version final problema 8

--- problema_8/main.cpp
+++ problema_8/main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 int imporigin(char *);
+int impcambios(char *);
 
 int main()
 {
@@ -9,6 +10,7 @@ int main()
     cout<<"Ingrese una cadena de caracteres: ";
     cin>>array;
     imporigin(array);
+    impcambios(array);
 }
 
 int imporigin(char *p)
@@ -24,3 +26,26 @@ int imporigin(char *p)
   }
   cout<<endl;
 }
+
+int impcambios(char *p)
+{
+    cout<<"Texto: ";
+    char carac=0;
+    int cont=0, a=0, num=0;
+    carac=*(p+cont);
+    while(carac!='\0'){
+        if (carac>47 && carac<58){
+            carac-=48;
+            a=carac;
+            num=num*10+a;
+        }
+        else{
+            cout<<carac;
+        }
+        cont+=1;
+        carac=*(p+cont);
+    }
+    cout<<endl<<"Numeros: "<<num<<endl;
+
+}
+
