bbdc5b30892d81e0a7dff74934fca74cddbfc212
Juan Caicedo <jesteban.caicedo@udea.edu.co>
2023-03-22 15:11:57 -0500

    version final problema 6

--- problema_6/main.cpp
+++ problema_6/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 
 using namespace std;
+int cambiamin(char *);
 
 int main()
 {
     char array[100];
     cout<<"Ingrese una palabra para cambiar sus minusculas: ";
     cin>>array;
+    cambiamin(array);
+}
+
+int cambiamin (char *p)
+{
+    char carac=0;
+    int cont=0;
+    carac=*(p+cont);
+    while(carac!='\0'){
+        if (carac>96 && carac<123){
+            carac-=32;
+        }
+        cout<<carac;
+        cont+=1;
+        carac=*(p+cont);
+    }
+    cout<<endl;
 
 }
