53fd8a86e9f17a68af0ab0c6c1e20219e939fdb0
Juan Caicedo <jesteban.caicedo@udea.edu.co>
2023-03-24 22:44:57 -0500

    version casi terminada problema 14

---
+++ problema_14/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+int** llenararre();
+void imprimir(int**);
+void matriznove(int**);
+void matrizochen(int**);
+void matrizseten(int**);
+
+
+int main()
+{
+    int **pointer=llenararre();
+    cout<<"La matriz inicial es:"<<endl;
+    imprimir(pointer);
+    matriznove(pointer);
+    cout<<"La matriz rotada noventa grados es: "<<endl;
+    imprimir(pointer);
+    cout<<"La matriz rotada ciento ochenta grados es: "<<endl;
+    matrizochen(pointer);
+    imprimir(pointer);
+
+
+
+    for(int i=0; i<5;i++){
+        delete[] pointer[i];
+    }
+    delete[] pointer;
+
+
+}
+
+void imprimir(int **pointer)
+{
+    for(int i=0; i<5;i++){
+        for(int j=0; j<5;j++){
+            if(*(*(pointer+i)+j)/10==0){
+                cout<<*(*(pointer+i)+j)<<"  ";
+            }
+            else{
+                cout<<*(*(pointer+i)+j)<<" ";
+            }
+        }
+    cout<<endl;
+    }
+}
+
+int** llenararre()
+{
+    int **p=new int *[5];
+    for (int i=0; i<5;i++) p[i]=new int[5];
+    int num=0;
+    for(int i=0; i<5;i++){
+        for(int j=0; j<5;j++){
+        num+=1;
+        *(*(p+i)+j)=num;
+        }
+    }
+    return p;
+}
+
+void matriznove(int **p)
+{
+    int num=0;
+    for (int i=4; i>=0;i--){
+        for(int j=0; j<5;j++){
+            num+=1;
+            *(*(p+j)+i)=num;
+        }
+    }
+}
+
+void matrizochen(int **p)
+{
+    int num=0;
+    for (int i=4; i>=0;i--){
+        for(int j=4; j>=0;j--){
+            num+=1;
+            *(*(p+i)+j)=num;
+        }
+    }
+}
+
+void matrizseten(int **p)
+{
+    int num=0;
+    for (int i=0; i<5;i++){
+        for(int j=4; j>=0;j--){
+            num+=1;
+            *(*(p+j)+i)=num;
+        }
+    }
+}
